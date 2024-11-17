313f569a0f027bc2abb99da5c1eb0d4d8dd6902c
maorobe14 <maorobe14@gmail.com>
2024-11-17 11:28:05 -0500

    versión inicial del microservicio code refactoring

---
+++ src/main/java/com/appgate/coderefactoring/CoderefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.coderefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderefactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderefactoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/coderefactoring/CoderefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.coderefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
